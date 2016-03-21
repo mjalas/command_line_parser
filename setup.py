18e707cb9ee6ced9c4ea7f8d5d37236db4d88a11
Mats Jalas <mats.jalas@gmail.com>
2016-03-21 16:06:27 +0200

    Added coverage to setup requirements

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ setup(
 
     # What does your project relate to?
     keywords='command-line-parsing argument-parsingoption-parsing',
-    setup_requires=['nose>=1.0'],
-    
+    setup_requires=['nose>=1.0', 'coverage>=4.0.3'],
+
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'env_creators']),
