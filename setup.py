761e89edbd503f09b1f8f570f6be5ee9fa10b89a
Mats Jalas <mats.jalas@gmail.com>
2017-01-24 20:02:41 +0200

    Updated setup files

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-long_description = ""
+#long_description = ""
 
-try:
-    from pypandoc import convert
-    if path.exists(path.join(here, 'README.md')):
-        long_description = convert('README.md', 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
+#try:
+#    from pypandoc import convert
+#    if path.exists(path.join(here, 'README.md')):
+#        long_description = convert('README.md', 'rst')
+#except ImportError:
+#    print("warning: pypandoc module not found, could not convert Markdown to RST")
 
 setup(
     name='command-line-parser',
     version='0.0.1',
     description='A customizable command line parser',
-    long_description=long_description,
+    #long_description=long_description,
     url='https://github.com/mjalas/command-line-parser',
     author='Mats Jalas',
-    author_email='',
+    author_email='mats.jalas@gmail.com',
     license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
